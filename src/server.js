074129879fbbe6d804801d66620013db536fc849
Gustavo Silva <gustavosilva@MacBook-Pro-de-Gustavo.local>
2020-03-11 11:44:11 -0300

    Add nodemon and create script yarn dev

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const server = express();
 
 server.get('/', (req, res) => {
     
-    return res.json({ message: req.query.name});
+    return res.json({ message: `Hello ${req.query.name}`});
 });
 
 server.listen(3333);

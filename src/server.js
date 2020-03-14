a02d9b85b49857c91e013fa38a6a6ac83c74411f
Gustavo Silva <gustavo@MacBook-Pro-de-Gustavo.local>
2020-03-14 02:46:33 -0300

    Import and use cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const routes = require('./routes');
+const cors = require('cors');
 
 const server = express();
 
@@ -9,9 +10,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-uezr2.mongodb.net/o
     useUnifiedTopology: true
 });
 
+
+server.use(cors());
 server.use(express.json());
-
-
 server.use(routes);
 
 
